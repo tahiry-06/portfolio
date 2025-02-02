f930ab07d99a98a16bd789170de9a8c67bca215b
tahiry-06 <tahiry.niaina06@gmail.com>
2025-02-02 22:28:05 +0300

    initialize files

--- js/main.js
+++ js/main.js
@@ -390,7 +390,7 @@ const choiceContent = document.querySelector(".choice-content");
 const choiceDirect = document.querySelector(".choice-direct");
 const choiceGumroad = document.querySelector(".choice-gumroad");
 
-const directURL = "assets/ressources/elevate-yourself.rar";
+const directURL = "assets/ressources/embrace-the-change.png";
 const gumroadURL = "https://tahir06.gumroad.com/l/elevateYourself";
 
 const gift = document.querySelector(".gift");
